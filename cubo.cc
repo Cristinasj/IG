93d834b0576456aa8cd21d465f082787e416c920
Cristinasj <cristinasj@protonmail.com>
2021-11-12 01:03:04 +0100

    First day on my journey to conquer IG

---
+++ cubo.cc
@@ -0,0 +1,46 @@
+#include "aux.h"
+#include "malla.h"
+#include "cubo.h"
+
+Cubo::Cubo(float lado)
+{
+
+   // inicializar la tabla de vértices
+		v.push_back(TuplaG3<float>(lado/2,lado/2,lado/2));
+	 	v.push_back(TuplaG3<float>(lado/2,lado/2,-lado/2));
+		v.push_back(TuplaG3<float>(lado/2,-lado/2,lado/2));
+		v.push_back(TuplaG3<float>(lado/2,-lado/2,-lado/2));
+		v.push_back(TuplaG3<float>(-lado/2,lado/2,lado/2));
+		v.push_back(TuplaG3<float>(-lado/2,lado/2,-lado/2));
+		v.push_back(TuplaG3<float>(-lado/2,-lado/2,lado/2));
+		v.push_back(TuplaG3<float>(-lado/2,-lado/2,-lado/2));
+
+   // inicializar la tabla de caras o triángulos:
+   // (es importante en cada cara ordenar los vértices en sentido contrario
+   //  de las agujas del reloj, cuando esa cara se observa desde el exterior del cubo)
+	 f.push_back(TuplaG3<unsigned>(1, 0, 2));
+	 f.push_back(TuplaG3<unsigned>(1, 2, 3));
+	 f.push_back(TuplaG3<unsigned>(0, 4, 6));
+	 f.push_back(TuplaG3<unsigned>(2, 0, 6));
+	 f.push_back(TuplaG3<unsigned>(6, 4, 5));
+	 f.push_back(TuplaG3<unsigned>(6, 5, 7));
+	 f.push_back(TuplaG3<unsigned>(2, 6, 7));
+	 f.push_back(TuplaG3<unsigned>(5, 1, 7));
+	 f.push_back(TuplaG3<unsigned>(7, 1, 3));
+	 f.push_back(TuplaG3<unsigned>(0, 5, 4));
+	 f.push_back(TuplaG3<unsigned>(0, 1, 5));
+	 f.push_back(TuplaG3<unsigned>(3, 2, 7));
+
+	// Inicializar los arrays de colores para los vértices 
+	// Modo puntos 
+	for (int i = 0; i < 8; i++) 
+		cpuntos.push_back(Tupla3f(0.99, 0.0, 0.0));
+	// Modo lineas 
+	for (int i = 0; i < 8; i++) 
+		clineas.push_back(Tupla3f(0.0, 0.99, 0.0));
+	// Modo relleno  
+	for (int i = 0; i < 8; i++) 
+		crelleno.push_back(Tupla3f(0.0, 0.0, 0.99));
+    glEnableClientState(GL_COLOR_ARRAY); 	
+
+}
---
+++ malla.cc
@@ -0,0 +1,81 @@
+#include "aux.h"
+#include "malla.h"
+
+// *****************************************************************************
+//
+// Clase Malla3D
+//
+// *****************************************************************************
+
+// Creación de las tablas VBO 
+GLuint CrearVBO (GLuint tipo_vbo, GLuint tamanio_bytes, 
+                  GLvoid * puntero_ram) {
+   GLuint id_vbo; 
+   glGenBuffers(1, & id_vbo); 
+   glBindBuffer(tipo_vbo, id_vbo); 
+   glBufferData(tipo_vbo, tamanio_bytes, puntero_ram, GL_STATIC_DRAW);
+   glBindBuffer(tipo_vbo, 0); 
+   return id_vbo;  
+
+}
+
+// Visualización en modo inmediato con 'glDrawElements'
+
+void Malla3D::draw_ModoInmediato()
+{
+  // visualizar la malla usando glDrawElements
+  // completar (práctica 1)
+  // Habilitar uso de un array de vértices
+  // glEnableClientState( GL_VERTEX_ARRAY);
+  // indicar el formato y la dirección de memoria del array de vértices
+  // (son tuplas de 3 valores float, sin espacio entre ellas)
+  // glVertexPointer(3, GL_FLOAT, 0, v.data());
+  // visualizar, indicando: tipo de primitiva,número de índices,
+  // tipo de los índices, y la dirección de la tabla de índices
+  // glDrawElements( GL_TRIANGLES, f.size()*3, GL_UNSIGNED_INT, f.data());
+  // deshabilitar array de vértices
+  // glDisableClientState(GL_VERTEX_ARRAY);
+   glEnableClientState(GL_VERTEX_ARRAY);
+   glVertexPointer(3,GL_FLOAT, 0, v.data());
+   glDrawElements(GL_TRIANGLES,3*f.size(),GL_UNSIGNED_INT, f.data());
+}
+// -----------------------------------------------------------------------------
+// Visualización en modo diferido con 'glDrawElements' (usando VBOs)
+
+void Malla3D::draw_ModoDiferido()
+{
+   // (la primera vez, se deben crear los VBOs y guardar sus identificadores en el objeto)
+   // completar (práctica 1)
+   // .....
+   // Activar VBO de vértices
+   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~glBindBuffer(GL_ARRAY_BUFFER, id_vbo_ver);
+   // Especifica formato y offset (=0)
+   glBindBuffer (GL_ARRAY_BUFFER, 0);
+   // Desactivar VBO de vértices
+   glBindBuffer(GL_ARRAY_BUFFER, 0);
+   // Habilitar tabla de vértices
+   glEnableClientState(GL_VERTEX_ARRAY);
+   // Visualizar triángulos con glDrawElements (puntero a tabla == 0)
+   // Activar VBO de triángulos
+   // ~~~~~~~ glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id_vbo_tri);
+   glDrawElements(GL_TRIANGLES, 3*f.size(), GL_UNSIGNED_INT, 0);
+   
+}
+// -----------------------------------------------------------------------------
+// Función de visualización de la malla,
+// puede llamar a  draw_ModoInmediato o bien a draw_ModoDiferido
+
+void Malla3D::draw(int modo)
+{
+   // completar .....(práctica 1)
+   switch(modo) {
+      case 1: 
+         draw_ModoInmediato(); 
+      break; 
+      case 2:
+         std::cout << "Entras en modo diferido" << std::endl;
+         draw_ModoDiferido();   
+      break; 
+   }
+
+}
