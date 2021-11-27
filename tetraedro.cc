26ce04d3ad60a125a6eb03ca2f9be27e6e3a8423
Cristinasj <cristinasj@protonmail.com>
2021-11-27 11:21:56 +0100

    Now the tetraedro is centered

--- tetraedro.cc
+++ tetraedro.cc
@@ -7,11 +7,12 @@ Tetraedro::Tetraedro()
    float lado = 100.0; 
 
    // inicializar la tabla de vértices
-		v.push_back(TuplaG3<float>(lado/2,0,cos(60.0*PI/180)*lado));
-	 	v.push_back(TuplaG3<float>(-lado/2,0,cos(60.0*PI/180)*lado));
-		v.push_back(TuplaG3<float>(0,0,lado));
-		v.push_back(TuplaG3<float>(0,lado,0));
-		
+	
+	v.push_back(TuplaG3<float>(lado/2,0,cos(60.0*PI/180)*lado));
+	v.push_back(TuplaG3<float>(-lado/2,0,cos(60.0*PI/180)*lado));
+	v.push_back(TuplaG3<float>(0,0,-lado));
+	v.push_back(TuplaG3<float>(0,lado,0));
+	
    // inicializar la tabla de caras o triángulos:
    // (es importante en cada cara ordenar los vértices en sentido contrario
    //  de las agujas del reloj, cuando esa cara se observa desde el exterior del cubo)
